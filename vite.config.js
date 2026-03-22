080bde8aa42b5013d060d65b5628d52a4c498a07
Guido <guido.pericotti@gmail.com>
2026-03-22 20:32:34 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  build: {
+    minify: 'terser', // Usa Terser para minificar el código
+    terserOptions: {
+      compress: {
+        drop_console: true,  // Elimina todos los console.*
+        drop_debugger: true, // Elimina todos los debugger.*
+      },
+    },
+  },
 })
